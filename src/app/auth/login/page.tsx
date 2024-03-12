95eccd66d7fe3f067e8bfb1e61f27333f7e6923b
bilalgursen <bilalgursen777@gmail.com>
2024-03-12 14:53:27 +0300

    Update login page layout and add cover image

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import Image from "next/image";
+
 const page = () => {
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -19,28 +22,38 @@ const page = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="h-screen flex flex-col items-center gap-3 justify-center">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" className="border" />
+      <div className="grid grid-cols-2 max-h-screen">
+        <Image
+          src={"/cover.png"}
+          className="object-cover max-h-full w-full"
+          width={720}
+          height={1024}
+          alt="Login Cover"
+        />
+
+        <form onSubmit={handleSubmit}>
+          <div className="h-screen flex flex-col items-center gap-3 justify-center">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" className="border" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" className="border" />
+            </div>
+            <div className="flex items-center justify-start ">
+              <input type="checkbox" className="gap-3" />
+              Remember me
+            </div>
+            <button
+              type="submit"
+              className="hover:bg-orange-600/50 bg-orange-600 px-2 py-1  text-white w-1/5"
+            >
+              Login
+            </button>
           </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" className="border" />
-          </div>
-          <div className="flex items-center justify-start ">
-            <input type="checkbox" className="gap-3" />
-            Remember me
-          </div>
-          <button
-            type="submit"
-            className="hover:bg-orange-600/50 bg-orange-600 px-2 py-1  text-white w-1/5"
-          >
-            Login
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     </>
   );
 };
