58b76b346c228d6420442ba1b94c98f3aec41b70
bilalgursen <bilalgursen777@gmail.com>
2024-03-14 16:35:28 +0300

    Added Header and Footer components to the layout.

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="h-[120px] w-full text-center">
+      All rights reserved. &copy; 2024
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/layout/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <nav className="h-[120px] w-full flex justify-evenly">
+      <div>Logo</div>
+      <div>Search</div>
+      <div>3 tabs</div>
+    </nav>
+  );
+};
+export default Header;
