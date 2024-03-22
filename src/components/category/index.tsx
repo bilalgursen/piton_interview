a0f9f811fce27f0381e16e51e8cb51bfb9746f1f
bilalgursen <bilalgursen777@gmail.com>
2024-03-22 14:10:02 +0300

    Version 2

---
+++ src/components/category/index.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import getCoverImage from "@/utils/get-cover-image";
+
+async function getProductsById(id: number) {
+  const res = await fetch(
+    `https://assign-api.piton.com.tr/api/rest/products/${id}`
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Category({ name = "Başlık Yok", id = 1 }) {
+  const products = await getProductsById(id);
+
+  return (
+    <div className="flex flex-col w-full px-12 mt-12">
+      <div className="mb-12">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-4xl font-medium">{name}</h2>
+          <Link className="text-xl font-medium text-[#EF6B4A]" href={name}>
+            View All
+          </Link>
+        </div>
+        <div className="grid grid-cols-4 gap-6">
+          {products.product.slice(0, 4).map((product: any, index: number) => (
+            <Link
+              key={index}
+              href={`/${name}/${product.name}`}
+              className="flex items-center bg-[#F4F4FF] border border-[#DCDCEB] rounded-md p-1 cursor-pointer"
+            >
+              <Image
+                src={getCoverImage(product.cover)}
+                alt={product.name}
+                width={500}
+                height={500}
+                className="w-32 h-48 object-cover"
+              />
+              <div className="flex flex-col h-full p-4 justify-between gap-3">
+                <div>
+                  <h3 className="text-xl font-medium">{product.name}</h3>
+                  <p className="text-sm text-gray-500">{product.author}</p>
+                </div>
+                <p className="text-2xl font-semibold text-[#6251DD]">
+                  {product.price} $
+                </p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
