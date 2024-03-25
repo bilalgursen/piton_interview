3801fc83dc077e03a6a69f0eb7f2d326b42437f3
bilalgursen <bilalgursen777@gmail.com>
2024-03-25 21:35:25 +0300

    merge v2 #1

--- src/utils/get-cover-image.ts
+++
@@ -1,3 +0,0 @@
-export default function getCoverImage(filename: string) {
-  return "https://s3.piton.com.tr/assignment/dune.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=P%21T10.MINIO%2F20240321%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240321T234346Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=246133f134b63fc9dd183b969cc6ea233759b2fe00a6b5feb3dffe31fabc3253";
-}
