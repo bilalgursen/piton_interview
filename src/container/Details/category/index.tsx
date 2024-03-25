0f77190ea03dc863534252ee23585037282bc261
bilalgursen <bilalgursen777@gmail.com>
2024-03-25 22:46:11 +0300

    Fix category details page

--- src/container/Details/category/index.tsx
+++ src/container/Details/category/index.tsx
@@ -38,25 +38,25 @@ export default async function CategoryDetailsContainer({
     <section className="px-4 sm:px-12 w-full mt-12">
       <TitleAndPrevious previous={"/"} title={name} />
       <div className="mt-12">
-        <div className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-6">
+        <div className="grid h-full xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-6">
           {products.map((product, index) => (
             <Link
               key={index}
               // İlk başta backend den gelen name'e uymak için slugify kullanıldı.
               href={`/${name}/${slugify(product?.slug, "-")}`}
-              className="flex flex-col items-center bg-[#F4F4FF] border border-[#DCDCEB] rounded-md p-1 w-full  cursor-pointer"
+              className="category-detail-card"
             >
               {" "}
-              <div>
+              <div className="w-full h-full">
                 <GetCoverImage filename={product?.cover} />
               </div>
-              <div className="flex w-full h-full px-5 justify-between items-end gap-3">
+              <div className="flex w-full justify-between items-end gap-3">
                 <div className="h-full flex flex-col justify-center">
                   <h3 className="text-xl font-medium">{product?.name}</h3>
                   <p className="text-sm text-gray-500">{product?.author}</p>
                 </div>
-                <p className="text-2xl font-semibold h-full flex items-center text-[#6251DD]">
-                  {product?.price} $
+                <p className="flex h-full text-items-end text-xl font-semibold  text-[#6251DD]">
+                  {product?.price}$
                 </p>
               </div>
             </Link>
